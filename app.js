3d741d9e77b517c25b6724ce13f0524d44d5061a
boripanc <boripan.c@gmail.com>
2022-11-12 23:38:02 +0700

    modified:   app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var generateRouter = require('./routes/generate');
 
 var app = express();
 
 
+app.use(cors())
+app.options('*', cors())
 app.use(logger('dev'));
 app.use(express.json());
 
